02918877dd1cdc3cde6e90159c79ec1f7aaf2087
田中建蔵 <tanakakenzou@tanakakenzounoMacBook.local>
2019-10-19 23:41:59 +0900

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `My Portfolio`,
-    author: `Mohan Talupula`,
+    title: `KEZO Dev Blog`,
+    author: `Kenzo Tanaka`,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi recusandae eos vel provident quod similique facilis error vero quisquam omnis?`,
     social: [
       {
         name: `Twitter`,
-        url: `https://twitter.com/gatsbyjs`
+        url: `https://twitter.com/kenzoooooB`
       },
       {
         name: `GitHub`,
-        url: `https://github.com/gatsbyjs`
+        url: `https://github.com/kenzoukenzou`
       }
     ]
   },
